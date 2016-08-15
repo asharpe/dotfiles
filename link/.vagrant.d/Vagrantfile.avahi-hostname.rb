fb9354f2ed507fc957d23dcab4e98716312743d4
Andrew Sharpe <asharpe@squiz.com.au>
2016-08-15 17:21:29 +1000

    updates for CentOS 7, only do RPM based installs

--- link/.vagrant.d/Vagrantfile.avahi-hostname.rb
+++ link/.vagrant.d/Vagrantfile.avahi-hostname.rb
@@ -5,6 +5,9 @@
 # in the nagios config...
 #
 
+epel_base = "http://mirror.aarnet.edu.au/pub/epel/6/i386"
+epel_base = "https://dl.fedoraproject.org/pub/epel"
+
 # may need to load the original first..
 if not defined? VagrantPlugins::GuestRedHat::Cap::ChangeHostName
 	require "vagrant"
@@ -31,13 +34,12 @@ end
 Vagrant.configure("2") do |config|
 	# I like using mdns for my VMs...
 	config.vm.provision :shell, :inline => <<-EOSH
-		flavour=$(lsb_release -si)
+		type yum >/dev/null 2>&1 || {
+			echo "YUM not available, not installing mDNS" >&2
+			exit
+		}
 
-		case $flavour in
-			Scientific|Red[Hh]at) ;;
-			*) echo "Not installing mDNS for $flavour"; exit;;
-		esac
-
+		# TODO this is too prescriptive
 		echo "Allowing mDNS traffic"
 		cat <<-EOF > /etc/sysconfig/iptables
 			*filter
@@ -58,11 +60,7 @@ Vagrant.configure("2") do |config|
 		[[ -f /etc/yum.repos.d/epel.repo ]] && skip=1 || skip=0
 
 		release_ver=$(rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release) | cut -d. -f1)
-
-		#type yum >&/dev/null && yum -y install http://mirror.aarnet.edu.au/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-		#type yum >&/dev/null && yum -y install http://mirror.aarnet.edu.au/pub/epel/epel-release-latest-6.noarch.rpm
-		#type yum >&/dev/null && yum -y install https://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-		type yum >&/dev/null && yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-${release_ver}.noarch.rpm
+		yum -y install git #{epel_base}/epel-release-latest-${release_ver}.noarch.rpm
 
 		((skip)) || {
 			# TODO this is fragile
@@ -96,9 +94,12 @@ Vagrant.configure("2") do |config|
 			rlimit-stack=4194304
 			rlimit-nproc=3
 		EOF
-		type yum >&/dev/null && yum -y --enablerepo=epel install nss-mdns && {
-			cp /etc/avahi/avahi-daemon.conf /etc/avahi/avahi-daemon.conf.orig
-			mv -f /tmp/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
+		yum -y --enablerepo=epel install nss-mdns && {
+			[[ -f /etc/avahi/avahi-daemon.conf.orig ]] || {
+				cp /etc/avahi/avahi-daemon.conf /etc/avahi/avahi-daemon.conf.orig
+				mv -f /tmp/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
+				type restorecon >/dev/null 2>&1 && restorecon /etc/avahi/avahi-daemon.conf
+			}
 			for service in messagebus avahi-daemon
 			do
 				chkconfig $service on
@@ -108,21 +109,45 @@ Vagrant.configure("2") do |config|
 			sed -i "/^hosts:/chosts:      files mdns4_minimal [NOTFOUND=return] dns mdns4" /etc/nsswitch.conf
 		}
 
+		function get_python_version() {
+			local version=$(rpm -qf $(which python) --qf "%{VERSION}")
+			local version_array=( ${version//./ } )
+
+			echo "${version_array[0]}.${version_array[1]}"
+		}
+
 		avahi_init=0
 		[[ -d /opt/avahi-alias ]] || {
 			echo "Setting up avahi cname support"
 			avahi_home=$(getent passwd avahi | awk -F: '{print $6}')
-			avahi_mount=$avahi_home/.local/lib/python2.6/site-packages
+			python_version=$(get_python_version)
+			avahi_mount=$avahi_home/.local/lib/python${python_version}/site-packages
 			site_packages=/opt/avahi-alias/site-packages
+
 			git clone --recursive https://github.com/asharpe/avahi-alias.git /opt/avahi-alias && {
-				cp /opt/avahi-alias/avahi-alias.upstart-sl6 /etc/init/avahi-alias.conf
-				mkdir -p $site_packages/avahi
-				ln -s /usr/lib/python2.6/site-packages/dbus $site_packages
-				ln -s /usr/lib64/python2.6/site-packages/_dbus_bindings.so $site_packages
+				# TODO CentOS 7 startup
+				[[ -d /etc/init ]] && cp /opt/avahi-alias/avahi-alias.upstart-sl6 /etc/init/avahi-alias.conf
+
+				mkdir -p $site_packages/avahi $avahi_mount /etc/avahi/alias.d
+
+				for f in \
+					/usr/lib/python${python_version}/site-packages/dbus \
+					/usr/lib64/python${python_version}/site-packages/dbus \
+					/usr/lib64/python${python_version}/site-packages/_dbus_bindings.so
+				do
+					[[ -e $f ]] && ln -s $f $site_packages/
+				done
+
 				curl -so $site_packages/avahi/__init__.py http://git.0pointer.net/avahi.git/plain/avahi-python/avahi/__init__.py
-				mkdir -p /etc/avahi/alias.d $avahi_mount
+
 				grep -q $site_packages /etc/fstab || {
-					echo "$site_packages	$avahi_mount none bind,defaults" >> /etc/fstab
+					echo "$site_packages	$avahi_mount none bind,defaults,noauto" >> /etc/fstab
+					cat <<-EOF >> /etc/rc.local
+						mkdir -p $avahi_mount
+						mount $avahi_mount
+					EOF
+
+
 					mount $avahi_mount
 					avahi_init=1
 				}
@@ -130,7 +155,27 @@ Vagrant.configure("2") do |config|
 		}
 
 		((avahi_init)) && {
-			start avahi-alias
+			# TODO detect client version
+			type systemctl >/dev/null 2>&1 && {
+				cat <<-EOF > /etc/systemd/system/avahi-alias.service
+					[Unit]
+					Description=Avahi alias
+					After=avahi.target
+
+					[Service]
+					ExecStart=/opt/avahi-alias/avahi-alias
+					Restart=on-abort
+					User=avahi
+
+					[Install]
+					WantedBy=multi-user.target
+				EOF
+				systemctl enable avahi-alias
+				systemctl start avahi-alias
+			} || {
+				echo "start avahi-alias" >> /etc/rc.local
+				start avahi-alias
+			}
 		}
 
 		cat <<-EOF >&2
