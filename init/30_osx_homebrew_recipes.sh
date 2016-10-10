97a027ede8e06b771ea1614331081a958bc8614c
Andrew Sharpe <EMAIL>
2016-10-10 20:23:19 +1100

    use system git

--- init/30_osx_homebrew_recipes.sh
+++ init/30_osx_homebrew_recipes.sh
@@ -6,19 +6,19 @@ is_osx || return 1
 
 # Homebrew recipes
 recipes=(
-  ansible
-  coreutils
-  git
-  htop-osx
-  lesspipe
-  macvim
-  netcat
-  nmap
-  pass
-  polipo
-  pstree
-  rocker
-  tree
+	ansible
+	coreutils
+#	git
+	htop-osx
+	lesspipe
+	macvim
+	netcat
+	nmap
+	pass
+	polipo
+	pstree
+	rocker
+	tree
 )
 
 brew_install_recipes
@@ -30,8 +30,8 @@ local binroot="$(brew --config | awk '/HOMEBREW_PREFIX/ {print $2}')"/bin
 
 # htop
 if [[ "$(type -P $binroot/htop)" ]] && [[ "$(stat -L -f "%Su:%Sg" "$binroot/htop")" != "root:wheel" || ! "$(($(stat -L -f "%DMp" "$binroot/htop") & 4))" ]]; then
-  e_header "Updating htop permissions"
-  sudo chown root:wheel "$binroot/htop"
-  sudo chmod u+s "$binroot/htop"
+	e_header "Updating htop permissions"
+	sudo chown root:wheel "$binroot/htop"
+	sudo chmod u+s "$binroot/htop"
 fi
 
