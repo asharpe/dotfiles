c937b9298224684cf3f3481c3793032c1186991a
Andrew Sharpe <EMAIL>
2016-10-11 19:37:40 +1100

    restructure and enable clipboard history

--- link/.hammerspoon/init.lua
+++ link/.hammerspoon/init.lua
@@ -1,21 +1,8 @@
+-- settings first
 --hs.window.setFrameCorrectness = true
 hs.window.animationDuration = 0
 
-function maximise()
-	local win = hs.window.focusedWindow()
-	local f = win:frame()
-	local screen = win:screen()
-	local max = screen:frame()
-
-	f.x = 0
-	f.y = 0
-	f.w = max.w
-	f.h = max.h
-
-	win:setFrame(f)
-end
-
-meta = {"cmd", "alt", "ctrl"}
-
-hs.hotkey.bind(meta, 'r', hs.reload)
-hs.hotkey.bind(meta, 'f', maximise)
+-- start the magic
+require "util"
+require "jumpcut"
+require "keys"
---
+++ link/.hammerspoon/keys.lua
@@ -0,0 +1,5 @@
+meta = {"cmd", "alt", "ctrl"}
+
+hs.hotkey.bind(meta, 'r', hs.reload)
+hs.hotkey.bind(meta, 'f', maximise)
+hs.hotkey.bind(meta, 'v', showClipboard)
